d515fbc1dce10fc449d609adba202a2a4ee1d42f
Yaroslav Poliakov <ypoliakov@playtika.com>
2018-01-11 17:26:13 +0200

    added score text

---
+++ Assets/Scripts/ScoreTextController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Signals;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+public class ScoreTextController : MonoBehaviour
+{
+    private int _scorepoints = 0;
+
+    [Inject]
+    public void Construct(GiveScorepointsSignal giveScorepointsSignal)
+    {
+        UpdateScoreText(_scorepoints);
+        giveScorepointsSignal += AddScorepoints;
+    }
+
+    private void AddScorepoints(int scorepoints)
+    {
+        _scorepoints += scorepoints;
+        UpdateScoreText(_scorepoints);
+    }
+
+    private void UpdateScoreText(int newScore)
+    {
+        GetComponent<TextMeshProUGUI>().text = $"Score: {newScore}";
+    }
+}
