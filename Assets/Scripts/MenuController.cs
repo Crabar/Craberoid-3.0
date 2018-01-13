c91001ce9e7dc9da111fad5cb7a71fba78a2e9b3
Crabar <crabar0@gmail.com>
2018-01-13 13:57:05 +0200

    added very basic start menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void OnStartGameButtonClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OnExitGameButtonClick()
+    {
+        Application.Quit();
+    }
+}
