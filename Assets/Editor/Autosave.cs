c0f2776dba5346fd7a7e6a0f6bcfb0d8616205a2
Yaroslav Poliakov <ypoliakov@playtika.com>
2018-02-05 19:17:02 +0200

    added autosave

---
+++ Assets/Editor/Autosave.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Editor
+{
+    [InitializeOnLoad]
+    public class Autosave
+    {
+        static Autosave()
+        {
+            EditorApplication.playModeStateChanged += e =>
+            {
+                if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+                {
+                    Debug.Log("Auto-saving all open scenes...");
+                    EditorSceneManager.SaveOpenScenes();
+                    AssetDatabase.SaveAssets();
+                }
+            };
+        }
+    }
+}
