53f34bfe6b845d277c1137d408a173bf1b232764
Yaroslav Poliakov <ypoliakov@playtika.com>
2018-02-05 11:38:32 +0200

    .

---
+++ Assets/Scripts/EndGameTextController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WinTextController : MonoBehaviour
+{
+    public void ShowWin(int totalScore)
+    {
+        GetComponent<TextMeshProUGUI>().text = $"You win!\nYour score is {totalScore}.";
+        GetComponent<Animator>().Play("WinTextAnimation");
+    }
+
+    public void ShowLose(int totalScore)
+    {
+        GetComponent<TextMeshProUGUI>().text = $"Game over!\nYour score is {totalScore}.";
+        GetComponent<Animator>().Play("WinTextAnimation");
+    }
+}
