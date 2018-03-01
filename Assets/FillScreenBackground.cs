529b8d4d97c30bcc49f869ac50122f5e2c1ce274
Yaroslav Poliakov <ypoliakov@playtika.com>
2018-03-01 16:41:56 +0200

    improved menu; added menu background;

---
+++ Assets/FillScreenBackground.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillScreenBackground : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var sizeY = Camera.main.orthographicSize;
+		var sizeX = (float)(sizeY * Camera.main.aspect / 1.6);
+		var maxScale = Math.Max(sizeX, sizeY);
+		transform.localScale = new Vector3(maxScale / 6, maxScale / 6, 1);
+	}
+}
